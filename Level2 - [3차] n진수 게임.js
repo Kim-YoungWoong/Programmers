50b7ef960fa1d610a726e2637c380d53e1b84678
novice-hero <highero10@kakao.com>
2022-06-22 16:17:33 +0900

    22-06-22 카카오 기출 문제

--- Level2 - [3차] n진수 게임.js
+++ Level2 - [3차] n진수 게임.js
@@ -6,7 +6,6 @@ function solution(n, t, m, p) {
     str += i.toString(n);
     i++;
   }
-  str.length > t * m ? str = str.slice(0, -1) : str;
 
   for (let i = 0; i < t * m; i++) {
     if (i % m === p - 1) answer += str[i];
